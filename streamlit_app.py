61cb16e70491ef7a2aeadeb105096c732d9f421d
daniel <cieslak.a.daniel@gmail.com>
2024-04-11 17:25:34 +0200

    req added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, get_audio_devices
 import speech_recognition as sr
-
 
 def recognize_speech_from_microphone(recognizer, microphone):
     with microphone as source:
@@ -20,10 +19,13 @@ def recognize_speech_from_microphone(recognizer, microphone):
         st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
         return None
 
-
 def listen_and_edit_text():
     recognizer = sr.Recognizer()
-    microphone = sr.Microphone()
+
+    audio_devices = get_audio_devices()
+    selected_audio_device = st.selectbox("Wybierz urządzenie audio", audio_devices)
+
+    microphone = sr.Microphone(device_index=selected_audio_device)
 
     text = ""
     text_area = st.empty()
@@ -34,10 +36,12 @@ def listen_and_edit_text():
         # Ustawiamy parametr `processing_timeout` na 10 sekund
         # żeby zakończyć nagrywanie po 10 sekundach nieaktywności
         processing_timeout=10,
+        audio_receiver_bitrate=16000,  # Dodajemy tę linijkę żeby ustawić bitrate na 16 kHz
+        rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
     )
 
     if webrtc_ctx.audio_receiver:
-        command = recognize_speech_from_microphone(recognizer, webrtc_ctx.audio_receiver)
+        command = recognize_speech_from_microphone(recognizer, microphone)
 
         if command:
             if "zakończ" in command.lower():
